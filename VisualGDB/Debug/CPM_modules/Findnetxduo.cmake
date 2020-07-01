55ce055d26120c5fdfc7051eb52457b8e128f696
Cindy Deng <t-xinden@microsoft.com>
2020-07-01 10:08:11 -0700

    remove VisualGDB directory

--- VisualGDB/Debug/CPM_modules/Findnetxduo.cmake
+++
@@ -1,3 +0,0 @@
-include(C:/Users/t-xinden/Desktop/Examples/AzureRTOS/getting-started/cmake/CPM.cmake)
-CPMAddPackage(NAME;netxduo;GIT_REPOSITORY;https://github.com/azure-rtos/netxduo.git;GIT_TAG;master)
-set(netxduo_FOUND TRUE)
