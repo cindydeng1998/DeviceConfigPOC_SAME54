3eade792948bf1c8f2733cd211cb38805641548f
Cindy Deng <xinze.deng@gmail.com>
2020-08-05 00:09:02 -0700

    remove out directory

--- out/build/x64-Debug (default)/CPM_modules/Findnetxduo.cmake
+++
@@ -1,3 +0,0 @@
-include(C:/Users/t-xinden/Desktop/Examples/Azure RTOS/getting-started/cmake/CPM.cmake)
-CPMAddPackage(NAME;netxduo;GIT_REPOSITORY;https://github.com/azure-rtos/netxduo.git;GIT_TAG;master)
-set(netxduo_FOUND TRUE)
--- out/build/x64-Debug (default)/CPM_modules/Findthreadx.cmake
+++
@@ -1,3 +0,0 @@
-include(C:/Users/t-xinden/Desktop/Examples/Azure RTOS/getting-started/cmake/CPM.cmake)
-CPMAddPackage(NAME;threadx;GIT_REPOSITORY;https://github.com/azure-rtos/threadx.git;GIT_TAG;master)
-set(threadx_FOUND TRUE)
