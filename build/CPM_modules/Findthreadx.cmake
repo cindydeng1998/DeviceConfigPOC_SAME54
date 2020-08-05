36e49efc79a2a18816d519a21871906d7a1f3622
Cindy Deng <xinze.deng@gmail.com>
2020-08-05 00:07:25 -0700

    remove unnecessary folders

--- build/CPM_modules/Findthreadx.cmake
+++
@@ -1,3 +0,0 @@
-include(C:/Users/t-xinden/Desktop/Examples/AzureRTOS/getting-started/cmake/CPM.cmake)
-CPMAddPackage(NAME;threadx;GIT_REPOSITORY;https://github.com/azure-rtos/threadx.git;GIT_TAG;master)
-set(threadx_FOUND TRUE)
